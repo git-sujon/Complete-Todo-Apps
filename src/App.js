fd405e09ab661fa1fb897e2f6d22f7a714d5f6f2
Mr-Robot-101 <git.sujon@gmail.com>
2022-10-08 09:45:20 +0600

    Todo Apps Added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Testtodo from './component/todoProject/testtodo';
+// import Tabs from "./component/TabMenu/Tabs";
+// import GalleryReact from "./component/TabMenu/GalleryReact";
+import Todo from './component/todoProject/todo';
+
+const App = () => {
+  return (
+    <>
+     {/* <GalleryReact /> */}
+     <Todo></Todo>
+     {/* <Testtodo></Testtodo> */}
+    </>
+  )
+}
+
+export default App
